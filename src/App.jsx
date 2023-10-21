7cae17791436b154564ed42b67e55f0e5239208c
Sean <soxfansean@gmail.com>
2023-10-21 13:54:15 -0400

    Connect frontend to webhook backend

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,35 @@ import React from 'react';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import './styles.css';
+import loading from './loading.gif';
 
+import { useEffect, useState } from 'react';
 function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    fetch("http://localhost:8000/webhook",
+    {
+      mode: "cors",
+      headers: {
+        accept: "application/json",
+        'Access-Control-Allow-Origin': 'http://localhost:3000/',
+        "origin": "*",
+        "content-type": "application/json",
+        'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS'
+      }
+    }
+    )
+    .then(res => res.json())
+    .then(res => setData(JSON.stringify(res)));
+  });
   return ( 
     <div>
       <Nav />
+      {data === "" ? <img src={loading} alt="loading" /> : 
+      <div>
+        {data}
+      </div>}
       <Footer />
     </div>
   );
