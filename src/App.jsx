7d1585c444e0414fa8869d5730e832da7960f059
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2023-10-21 14:22:11 -0400

    added Parse.js functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import Login from './components/Login'
+import Parse from "./functions/Parse"
 import './styles.css';
 
+// Firebase imports
+import { usersCollection } from './firebase'
+import { addDoc, getDocs, updateDoc, onSnapshot, collection } from 'firebase/firestore'
+import { auth } from "./firebase"
+
+
 function App() {
-  return ( 
-    <div>
-      <Nav />
-      <Footer />
-    </div>
-  );
+    // check if user is authenticated (decides what page to render)
+    const [isAuthenticated, setIsAuthenticated] = React.useState(false)
+
+    const user = auth.currentUser
+
+
+    Parse()
+    return ( 
+        <div>
+            <Nav />
+            <Footer />
+            {isAuthenticated || <Login />}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,35 @@
+
+import React from 'react'
+import { auth } from '../firebase'
+import { GoogleAuthProvider, signInWithPopup} from "firebase/auth"
+import { usersCollection } from '../firebase'
+
+
+export default function Login() {
+
+    // function to signInWithGoogle (using Firebase authentication)
+    const signInWithGoogle = () => {
+		// initialize provider
+		const provider = new GoogleAuthProvider()
+
+		// use Firebase sign in function
+		signInWithPopup(auth, provider)
+			.then((result) => {
+				console.log(result)
+			})
+			.catch((error) => {
+				console.log(error);
+			})
+      }
+    
+    return (
+        <div className='login'>
+            <button 
+                className='google-btn'
+                onClick={signInWithGoogle}
+            >
+                Sign In With Google
+            </button>
+        </div>
+    )
+}
