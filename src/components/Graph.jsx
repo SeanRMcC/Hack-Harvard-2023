aa15fcf35c545101ed0d77dcc8e1431093fe390d
aljarida <jarida@bu.edu>
2023-10-21 22:40:47 -0400

    revert graphs

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,18 +1,27 @@
+import React from 'react';
+
+// Import your graph functions here
 import { lineGraphDays30, lineGraphDays14, barGraphDays7, barGraphDays3 } from './GraphCalls.jsx';
 
+const user_id = "d55d0a60-c89d-4bd7-a113-b78ee1f79dfa";
+
 function Graph() {
-    // Replace with unique user_ID
-    const user_id = "5f4c5ac9-c2c5-4bfd-9f98-9d810ceb4af5"
-    return (
-        <div className='graph'>
-            <ul>
-                <li>{lineGraphDays30(user_id, 500, 500)}</li>
-                <li>{lineGraphDays14(user_id, 500, 500)}</li>
-                <li>{barGraphDays7(user_id, 500, 500)}</li>
-                <li>{barGraphDays3(user_id, 500, 500)}</li>
-            </ul>
-        </div>
-    )
+  return (
+    <div className="graph-container">
+      <div className="graph-item">
+        {lineGraphDays30(user_id, 550, 550)}
+      </div>
+      <div className="graph-item">
+        {lineGraphDays14(user_id, 550, 550)}
+      </div>
+      <div className="graph-item">
+        {barGraphDays7(user_id, 550, 550)}
+      </div>
+      <div className="graph-item">
+        {barGraphDays3(user_id, 550, 550)}
+      </div>
+    </div>
+  );
 }
 
 export default Graph;
